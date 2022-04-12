ac3180b6245b6a897e283aaa4322db9cd1aa3813
Pierre Sicallac <pierre.sicallac@hotmail.fr>
2022-04-12 14:23:33 +0200

    We can now play.

--- room_game.py
+++ room_game.py
@@ -1,3 +1,4 @@
+import os
 from random import randint
 
 class Rooms:
@@ -15,7 +16,6 @@ class Rooms:
 			self.number_path = self.link_rooms()
 
 
-
 	def generate_rooms(self):
 		"""Generate the rooms dict"""
 		return {i:[0 for z in range(self.n_connect)] for i in range(self.numbers)}
@@ -28,6 +28,7 @@ class Rooms:
 				self.rooms[x][y] = randint(0, self.numbers - 1)
 				self.rooms[self.rooms[x][y]][randint(0, self.n_connect - 1)] = x
 		return self.dfs(self.rooms, self.start_room, frozenset(), self.finish_room)
+
 
 	def set_finish_room(self):
 		"""Random finish room we just check finish != start room"""
@@ -46,6 +47,7 @@ class Rooms:
 		"""Define the bonus rooms"""
 		pass
 
+
 	def dfs(self, g, node, visit, finish_room):
 		"""Deep first search algo for check if a path exist"""
 		if node == finish_room:
@@ -56,10 +58,31 @@ class Rooms:
 			visit = visit | {node}
 		return sum(self.dfs(g, n, visit, finish_room) for n in g[node])
 
-if __name__ == "__main__":
+
+def main():
 	# It's hard to put more than 40 rooms beacause after dfs algo it's too long to find one all path
-	r = Rooms(40)
-	print(f'Start room: {r.start_room}, finish room: {r.finish_room}')
-	print(f'Number of path: {r.number_path}')
-	for x in range(r.numbers):
-		print(f'Room {x} link to {r.rooms[x]}')
+	room = Rooms(10)
+	position = room.start_room
+	while position != room.finish_room:
+		os.system('cls' if os.name == 'nt' else 'clear')
+		print(f"""You are in the room {position}.
+They are three doors in front of you : {room.rooms[position][0]}, {room.rooms[position][1]}, {room.rooms[position][2]}.""")
+		try:
+			v = int(input('What door you choose ? '))
+			if v in room.rooms[position]:
+				position = v
+			else:
+				pass
+		except:
+			pass
+
+	if position == room.finish_room:
+		print("Congratulation, you find the finish room !")
+		choice = input("Play again ? [Y/n] ")
+		if choice == "Y":
+			main()
+		else:
+			exit()
+
+if __name__ == "__main__":
+	main()
